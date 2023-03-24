92e6d73fe5a90b4834ccb7c361b35fc19c87af0a
kevin <kevin.engrand7@gmail.com>
2023-03-24 11:29:23 +0100

    modif précalcul simulation QuoromD + resert dans la fonction start

--- simulation/simulationQuoromD.cpp
+++ simulation/simulationQuoromD.cpp
@@ -10,7 +10,7 @@ SimulationQuoromD::SimulationQuoromD(int *T, int nbTest, double *probaDef, Voisi
     this->v = v;
     this->lambda = lambda;
     this->nbTest = nbTest;
-    this->G = new Grille(width, height,typeCellue::QUOROMD);
+    this->G = new Grille(width, height, typeCellue::QUOROMD);
     this->G->setVoisinage(v);
     this->listExp = nullptr;
 }
@@ -23,7 +23,7 @@ void SimulationQuoromD::start() {
         for (int a = 1; a <= lambda[0]; a++) {
             //précalcule des différentes valeurs de probabilité pour QuoromD
             if (listExp != nullptr) free(listExp);
-            calculeExp(lambda[i],v);
+            calculeExp(lambda[i], v);
 
             this->openFile("QuoromDLambda" + to_string(lambda[a]) + "T" + to_string(T[i]) + ".txt");
 
@@ -63,13 +63,13 @@ void SimulationQuoromD::simulationLambda() {
     int nbIter;
     int succes;
     for (int a = 1; a <= T[0]; a++) {
-        this->openFile("QuoromD" + ToString(v) + "LambdamultipleT"+to_string(T[a])+".txt");
+        this->openFile("QuoromD" + ToString(v) + "LambdamultipleT" + to_string(T[a]) + ".txt");
         if (this->file) { // si l'ouverture a réussi
             this->writeFile(to_string(T[0]));
             for (int i = 1; i <= lambda[0]; i++) {
                 //précalcule des différentes valeurs de probabilité pour QuoromD
                 if (listExp != nullptr) free(listExp);
-                calculeExp(lambda[i],v);
+                calculeExp(lambda[i], v);
                 this->writeFile(to_string(lambda[i]));
                 for (int j = 1; j <= probaDef[0]; j++) {
                     succes = 0;
@@ -105,7 +105,7 @@ void SimulationQuoromD::simulationT() {
     for (int a = 1; a <= lambda[0]; a++) {
         //précalcule des différentes valeurs de probabilité pour QuoromD
         if (listExp != nullptr) free(listExp);
-        calculeExp(lambda[a],v);
+        calculeExp(lambda[a], v);
         //création de fichier
         this->openFile("QuoromD" + ToString(v) + "Lambda" + to_string(lambda[a]) + "Tmultiple.txt");
         if (this->file) { // si l'ouverture a réussi
@@ -131,7 +131,7 @@ void SimulationQuoromD::simulationT() {
                             nbIter++;
                         }
                     }
-                    
+
                     //on écrit le probabilité testé, la proba de réussite et le nombre d'itération pour la derniére simulation
                     string s = to_string(probaDef[j]) + "," + to_string((double) succes / (double) nbTest) + "," +
                                to_string(nbIter);
@@ -152,13 +152,18 @@ void SimulationQuoromD::startDensitySim(Focus f) {
 
 void SimulationQuoromD::simulationDensityT() {
     for (int a = 1; a <= lambda[0]; a++) {
+        //précalcule des différentes valeurs de probabilité pour QuoromD
+        if (listExp != nullptr) free(listExp);
+        calculeExp(lambda[a], v);
+
         for (int j = 1; j <= probaDef[0]; j++) {
 
-        this->openFile("QuoromDDensity"+ ToString(v)+"Lambda" + to_string(lambda[a]) + "TMultipleProbaDef"+to_string(probaDef[j]) +".txt");
-        this->writeFile(to_string(T[0]));
+            this->openFile("QuoromDDensity" + ToString(v) + "Lambda" + to_string(lambda[a]) + "TMultipleProbaDef" +
+                           to_string(probaDef[j]) + ".txt");
+            this->writeFile(to_string(T[0]));
 
-        for (int i = 1; i <= T[0]; i++) {
-            this->writeFile(to_string(T[i]));
+            for (int i = 1; i <= T[0]; i++) {
+                this->writeFile(to_string(T[i]));
 
                 if (this->file) { // si l'ouverture a réussi
 
@@ -191,7 +196,7 @@ void SimulationQuoromD::simulationDensityT() {
                         }
                     }
                 }
-            this->writeFile("-");
+                this->writeFile("-");
             }
             this->closeFile();
         }
@@ -200,9 +205,14 @@ void SimulationQuoromD::simulationDensityT() {
 
 void SimulationQuoromD::simulationDensityProbadef() {
     for (int a = 1; a <= lambda[0]; a++) {
+        //précalcule des différentes valeurs de probabilité pour QuoromD
+        if (listExp != nullptr) free(listExp);
+        calculeExp(lambda[a], v);
+
         for (int i = 1; i <= T[0]; i++) {
 
-            this->openFile("QuoromDDensity"+ ToString(v)+"LambdaMultipleT"+to_string(T[i])+"ProbaDefMultiple.txt");
+            this->openFile(
+                    "QuoromDDensity" + ToString(v) + "LambdaMultipleT" + to_string(T[i]) + "ProbaDefMultiple.txt");
             this->writeFile(to_string(probaDef[0]));
 
             for (int j = 1; j <= probaDef[0]; j++) {
@@ -247,13 +257,17 @@ void SimulationQuoromD::simulationDensityProbadef() {
 }
 
 void SimulationQuoromD::simulationDensityLambda() {
-        for (int j = 1; j <= probaDef[0]; j++) {
-            for (int i = 1; i <= T[0]; i++) {
+    for (int j = 1; j <= probaDef[0]; j++) {
+        for (int i = 1; i <= T[0]; i++) {
 
-            this->openFile("QuoromDDensity"+ ToString(v)+"LambdaMultipleT"+to_string(T[i])+"ProbaDef"+to_string(probaDef[j]) +".txt");
+            this->openFile("QuoromDDensity" + ToString(v) + "LambdaMultipleT" + to_string(T[i]) + "ProbaDef" +
+                           to_string(probaDef[j]) + ".txt");
             this->writeFile(to_string(lambda[0]));
 
-                for (int a = 1; a <= lambda[0]; a++) {
+            for (int a = 1; a <= lambda[0]; a++) {
+                //précalcule des différentes valeurs de probabilité pour QuoromD
+                if (listExp != nullptr) free(listExp);
+                calculeExp(lambda[a], v);
                 this->writeFile(to_string(lambda[a]));
 
                 if (this->file) { // si l'ouverture a réussi
@@ -297,34 +311,40 @@ void SimulationQuoromD::simulationDensityLambda() {
 void SimulationQuoromD::startDensitySim() {
     for (int i = 1; i <= T[0]; i++) {
         for (int a = 1; a <= lambda[0]; a++) {
+            //précalcule des différentes valeurs de probabilité pour QuoromD
+            if (listExp != nullptr) free(listExp);
+            calculeExp(lambda[a], v);
             for (int j = 1; j <= probaDef[0]; j++) {
-                this->openFile("QuoromDDensity"+ ToString(v)+"Lambda" + to_string(lambda[a]) + "T" + to_string(T[i]) +"ProbaDef"+to_string(probaDef[j]) +".txt");
+                this->openFile(
+                        "QuoromDDensity" + ToString(v) + "Lambda" + to_string(lambda[a]) + "T" + to_string(T[i]) +
+                        "ProbaDef" + to_string(probaDef[j]) + ".txt");
                 this->writeFile(to_string(1));
                 if (this->file) { // si l'ouverture a réussi
-                    G->Graphe::reset();
-                    G->setVoisinage(v);
-                    G->setCelluleDef(probaDef[i]);
-                    G->setLamba(lambda[a]);
 
-                    string s = to_string(0) + "," + to_string((double)G->nbAlerte()/((double)(G->nbCellule()-G->nbDef())));
+                    //On réinitialise le graphe correctement
+                    G->reset(probaDef[i], lambda[a], listExp);
+
+                    string s = to_string(0) + "," +
+                               to_string((double) G->nbAlerte() / ((double) (G->nbCellule() - G->nbDef())));
                     this->writeFile(s);
 
                     int nbIter = 1;
-                    int cpt =11;
+                    int cpt = 11;
                     for (int k = 0; k < nbTest; k++) {
                         while (nbIter < T[i]) {
                             G->MAJGrille();
-                            if (cpt < 10){
+                            if (cpt < 10) {
                                 cpt++;
                                 if (cpt == 10) nbIter = T[i];
-                            }
-                            else if (G->seuil(threshold)) {
-                                string s = to_string(nbIter) + "," + to_string((double)G->nbAlerte()/((double)(G->nbCellule()-G->nbDef())));
+                            } else if (G->seuil(threshold)) {
+                                string s = to_string(nbIter) + "," +
+                                           to_string((double) G->nbAlerte() / ((double) (G->nbCellule() - G->nbDef())));
                                 this->writeFile(s);
-                                cpt =0;
+                                cpt = 0;
                                 break;
                             }
-                            string s = to_string(nbIter) + "," + to_string((double)G->nbAlerte()/((double)(G->nbCellule()-G->nbDef())));
+                            string s = to_string(nbIter) + "," +
+                                       to_string((double) G->nbAlerte() / ((double) (G->nbCellule() - G->nbDef())));
                             this->writeFile(s);
                             nbIter++;
                         }
@@ -343,15 +363,15 @@ void SimulationQuoromD::calculeExp(double lambda, Voisinage v) {
     else if (v == Voisinage::TOOM) nbVoisin = 3;
     else if (v == Voisinage::VONNEUMAN) nbVoisin = 4;
 
-    this->listExp = (double*)malloc(sizeof(double) * (nbVoisin+1) * (nbVoisin+1));
+    this->listExp = (double *) malloc(sizeof(double) * (nbVoisin + 1) * (nbVoisin + 1));
     // nombre de cellules voisinnes normales
-    for (int i =0; i <= nbVoisin; i++){
+    for (int i = 0; i <= nbVoisin; i++) {
         //nombre de cellules voisines
-        for( int j = 1; j <= nbVoisin; j++){
-            double wn =  exp(lambda * ((double)i/j));
-            double wad = exp(lambda * ((double)(j-i)/j));
-            int position = j*(nbVoisin+1) + i;
-            this->listExp[position] = wn/(wn+wad);
+        for (int j = 1; j <= nbVoisin; j++) {
+            double wn = exp(lambda * ((double) i / j));
+            double wad = exp(lambda * ((double) (j - i) / j));
+            int position = j * (nbVoisin + 1) + i;
+            this->listExp[position] = wn / (wn + wad);
 
             //cout << "i:"<< i <<" j:" << j << " v:"<< this->listExp[(j*(nbVoisin+1)) + i]<< endl;
         }
